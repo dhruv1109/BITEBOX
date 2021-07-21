a56ae4d86d253bead8aa4f1cd104290e494121af
dhruv1109 <56334865+dhruv1109@users.noreply.github.com>
2021-07-21 23:34:35 +0530

    ur

--- src/Components/Dashboard/Reasons.js
+++ src/Components/Dashboard/Reasons.js
@@ -14,6 +14,7 @@ const Reasons = () => {
                  <img  src="https://image.freepik.com/free-vector/delivery-man-riding-red-scooter-illustration_9845-14.jpg" alt={logo} className="card-img-top  mx-auto img-thumbnail" style={{maxWidth: 250, height: 200}}/>
                       <div className="card-body">
                       <h5 className="card-title">Free Delivery </h5>
+                      
                       <div className="card-text">Tired of a whole days work don't worry we got you covered, Bitebox gives free delivery above a minimum order.</div>
                     </div>
                   </div>
@@ -23,6 +24,7 @@ const Reasons = () => {
                  <img  src="https://image.freepik.com/free-vector/pastry-chefs-decorating-big-wedding-birthday-cake_74855-7772.jpg" alt={logo} className="card-img-top  mx-auto img-thumbnail" style={{maxWidth: 250, height: 200}}/>
                       <div className="card-body">
                       <h5 className="card-title">Birthday Parties</h5>
+
                       <div className="card-text">Order food in bulk and get amazing offers from Bitebox HAPPY BIRTHDAY!!.</div>
     
                     </div>
@@ -35,6 +37,7 @@ const Reasons = () => {
                     <img  src="https://image.freepik.com/free-vector/boy-studying-with-book_113065-238.jpg" alt={logo} className="card-img-top  mx-auto img-thumbnail" style={{maxWidth: 250, height: 200}}/>
                       <div className="card-body">
                       <h5 className="card-title">Late Night Hunger </h5>
+
                       <div className="card-text">We all have been there studying late night for exams, and you start to feel hungry not to worry Bitebox will serve you from all the available Restraunts.</div>
                     </div>
                   </div>
@@ -44,6 +47,7 @@ const Reasons = () => {
                  <img  src="https://image.freepik.com/free-vector/illustrated-types-face-masks_23-2148604345.jpg" alt={logo} className="card-img-top  mx-auto img-thumbnail" style={{maxWidth: 250, height: 200}}/>
                       <div className="card-body">
                       <h5 className="card-title">Post Covid Era</h5>
+
                       <div className="card-text">Today's scenario is not that safe for large gatherings and to avoid that in public ,you can use our App for online ordering .</div>
     
                     </div>
